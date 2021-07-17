8d956d9d0bce8332a2f62098446e7e01891c56d4
0l3h <kubik7950@gmail.com>
2021-07-18 00:22:27 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+printNumbersInterval(1, 100, 10);
+
+/**
+ * Displays a number of a given range after fixed amount of time using setInterval method.
+ * 
+ * @param {Number} from       First number in range.
+ * @param {Number} to         Last number in range.
+ * @param {Number} interval   Time interval in milliseconds beetween function calls. 
+ */
+function printNumbersInterval(from, to, interval) {
+    let timerId = setInterval(() => {
+        console.log(from++);
+        if(from === to) {
+            clearInterval(timerId);
+        }
+    }, interval);
+}
+
+printNumbersTimeout(1, 100, 10);
+
+/**
+ * Displays a number of a given range after fixed amount of time using recursive setTimeout method.
+ * 
+ * @param {Number} from       First number in range.
+ * @param {Number} to         Last number in range.
+ * @param {Number} interval   Time interval in milliseconds beetween function calls. 
+ */
+function printNumbersTimeout(from, to, interval) {
+    console.log(from++);
+    
+    let timerId = setTimeout(printNumbersTimeout, interval, from, to, interval);
+    
+    if(from === to) {
+        clearTimeout(timerId);
+    }
+}
